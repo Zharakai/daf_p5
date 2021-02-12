53269f71049afe165bab815a9dba80a8be965dc6
unknown <yannis.almanric@gmail.com>
2021-02-12 17:04:26 +0100

    Integrate header html + css

--- app.js
+++ app.js
@@ -1,1 +1,60 @@
-console.log("coucou");
+// Generator 1
+let generatorOneStart = [
+    "generatorOneStart_1",
+    "generatorOneStart_2",
+    "generatorOneStart_3",
+    "generatorOneStart_4",
+    "generatorOneStart_5"
+];
+let generatorOneMiddle = [
+    "generatorOneMiddle_1",
+    "generatorOneMiddle_2",
+    "generatorOneMiddle_3",
+    "generatorOneMiddle_4",
+    "generatorOneMiddle_5"
+];
+let generatorOneEnd = [
+    "generatorOneEnd_1",
+    "generatorOneEnd_2",
+    "generatorOneEnd_3",
+    "generatorOneEnd_4",
+    "generatorOneEnd_5",
+];
+
+// Generator 2
+let generatorTwoStart = [
+    "generatorTwoStart_1",
+    "generatorTwoStart_2",
+    "generatorTwoStart_3",
+    "generatorTwoStart_4",
+    "generatorTwoStart_5",
+];
+let generatorTwoMiddle = [
+    "generatorTwoMiddle_1",
+    "generatorTwoMiddle_2",
+    "generatorTwoMiddle_3",
+    "generatorTwoMiddle_4",
+    "generatorTwoMiddle_5",
+];
+let generatorTwoEnd = [
+    "generatorTwoEnd_1",
+    "generatorTwoEnd_2",
+    "generatorTwoEnd_3",
+    "generatorTwoEnd_4",
+    "generatorTwoEnd_5",
+];
+
+[
+    {
+      phraseType: 'Vie de tous les jours',
+      sujets: ['Stan', 'Yannis', 'Mon patron'],
+      verbes: ['a cassé', 'viens de me rejoindre', 'bois du café'],
+      complements: ['au bar', 'avec sa copine'],
+    }
+    {
+      phraseType: 'Films',
+      sujets: ['Stan', 'Yannis', 'Mon patron'],
+      verbes: ['a cassé', 'viens de me rejoindre', 'bois du café'],
+      complements: ['au bar', 'avec sa copine'],
+    }
+  ]
