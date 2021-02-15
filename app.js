a1649e05071fba41b4ef9920290fe9012190f500
unknown <yannis.almanric@gmail.com>
2021-02-15 15:46:32 +0100

    Generate quotes 1 to 5 with two types

--- app.js
+++ app.js
@@ -1,60 +1,68 @@
-// Generator 1
-let generatorOneStart = [
-    "generatorOneStart_1",
-    "generatorOneStart_2",
-    "generatorOneStart_3",
-    "generatorOneStart_4",
-    "generatorOneStart_5"
-];
-let generatorOneMiddle = [
-    "generatorOneMiddle_1",
-    "generatorOneMiddle_2",
-    "generatorOneMiddle_3",
-    "generatorOneMiddle_4",
-    "generatorOneMiddle_5"
-];
-let generatorOneEnd = [
-    "generatorOneEnd_1",
-    "generatorOneEnd_2",
-    "generatorOneEnd_3",
-    "generatorOneEnd_4",
-    "generatorOneEnd_5",
+// Subjects, verbs, complements
+let generator = [
+    {
+      type: "Reflet d'Acide",
+      subjects: ['Wrandrall', 'Zehirmahnn ', 'Zarakaï', 'Enoriel', 'Trichelieu', 'Guertrude'],
+      verbs: ['se bat avec une épée rouillée', 'chante Pom... Pom pom', 'se bat avec une énorme hache', 'boit une pinte de bière bien fraîche'],
+      complements: ['au bar.', 'avec sa fiancée.', 'avec ses compagnons.'],
+    },
+    {
+      type: 'Le petit chaperon rouge',
+      subjects: ['Le petit chaperon rouge', 'Le chasseur', 'Le loup'],
+      verbs: ['part visiter sa Grand mère', 'doit traverser le village en passant par la forêt', 'a déjà dévoré la grand-mère et a pris sa place dans son lit', 'tire la chevillette'],
+      complements: ['avec une galette et un petit pot de beurre.', 'et se fait dévorer.', 'et la bobinette cherra.'],
+    }
 ];
 
-// Generator 2
-let generatorTwoStart = [
-    "generatorTwoStart_1",
-    "generatorTwoStart_2",
-    "generatorTwoStart_3",
-    "generatorTwoStart_4",
-    "generatorTwoStart_5",
-];
-let generatorTwoMiddle = [
-    "generatorTwoMiddle_1",
-    "generatorTwoMiddle_2",
-    "generatorTwoMiddle_3",
-    "generatorTwoMiddle_4",
-    "generatorTwoMiddle_5",
-];
-let generatorTwoEnd = [
-    "generatorTwoEnd_1",
-    "generatorTwoEnd_2",
-    "generatorTwoEnd_3",
-    "generatorTwoEnd_4",
-    "generatorTwoEnd_5",
-];
+// Get a random number between 0 and max
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
-[
-    {
-      phraseType: 'Vie de tous les jours',
-      sujets: ['Stan', 'Yannis', 'Mon patron'],
-      verbes: ['a cassé', 'viens de me rejoindre', 'bois du café'],
-      complements: ['au bar', 'avec sa copine'],
+let divForQuotes = document.getElementsByClassName('displayQuotes');
+let quotesList = document.getElementById('quotesList');
+let blockquote = [];
+let chosenValue;
+
+function createBlockQuote(text) {
+    let blockQuote = document.createElement('blockquote');
+    blockQuote.textContent = text;
+    return blockQuote;
+}
+
+function appendChildren(parent, children) {
+    children.forEach(function(child) {
+        parent.appendChild(child);
+    });
+}
+
+document.getElementById('button_1').addEventListener('click', function() {
+    blockquote = [];
+    console.log(blockquote);
+    quotesList.innerHTML = '';
+    chosenValue = document.getElementById('numberChoice').value;
+
+    for (let i = 0; i < chosenValue; i++) {
+        blockquote.push(createBlockQuote(`
+        ${generator[0].subjects[getRandomInt(generator[0].subjects.length)]} 
+        ${generator[0].verbs[getRandomInt(generator[0].verbs.length)]} 
+        ${generator[0].complements[getRandomInt(generator[0].complements.length)]}
+        `));
     }
-    {
-      phraseType: 'Films',
-      sujets: ['Stan', 'Yannis', 'Mon patron'],
-      verbes: ['a cassé', 'viens de me rejoindre', 'bois du café'],
-      complements: ['au bar', 'avec sa copine'],
+    appendChildren(quotesList, blockquote);
+});
+
+document.getElementById('button_2').addEventListener('click', function() {
+    blockquote = [];
+    quotesList.innerHTML = '';
+    chosenValue = document.getElementById('numberChoice').value;
+
+    for (let i = 0; i < chosenValue; i++) {
+        blockquote.push(createBlockQuote(`
+        ${generator[1].subjects[getRandomInt(generator[1].subjects.length)]} 
+        ${generator[1].verbs[getRandomInt(generator[1].verbs.length)]} 
+        ${generator[1].complements[getRandomInt(generator[1].complements.length)]}
+        `));
     }
-  ]
+    appendChildren(quotesList, blockquote);
+});
